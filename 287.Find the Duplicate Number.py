e644c23e675bfa660a5078620dc27940545228ce
Aravindan.S <aravindan.s.official@gmail.com>
2026-01-06 22:32:49 +0530

    287.Find the Duplicate Number.py
    Refactor findDuplicate method to improve readability.

--- 287.Find the Duplicate Number.py
+++ 287.Find the Duplicate Number.py
@@ -2,7 +2,7 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow = nums[0]
         fast = nums[0]
-
+        
         while True :
             slow = nums[slow]
             fast = nums[nums[fast]]
